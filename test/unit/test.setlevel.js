f2c027825b11daf73609e2c24a6d5d9b6d6c65f7
David Keijser <keijser@gmail.com>
2015-07-01 15:56:50 +0200

    convert tests to tape

--- test/unit/test.setlevel.js
+++ test/unit/test.setlevel.js
@@ -1,18 +1,19 @@
-describe('setLevel', function() {
-  var levels = require('../../lib/levels')
+var test = require('tape')
+  , levels = require('../../lib/levels')
 
-  function Target() {}
-  Target.prototype.setLevel = levels.setLevel
+function Target() {}
+Target.prototype.setLevel = levels.setLevel
 
-  it('updates the level of the object', function() {
-    var target = new Target()
-    target.setLevel(20)
-    assert.equal(target.level, 20)
-  })
+test('setLevel updates the level of the object', function(t) {
+  t.plan(1)
+  var target = new Target()
+  target.setLevel(20)
+  t.equal(target.level, 20)
+})
 
-  it('updates the level of the object from symbolic name', function() {
-    var target = new Target()
-    target.setLevel('DEBUG')
-    assert.equal(target.level, 10)
-  })
+test('setLevel updates the level of the object from symbolic name', function(t) {
+  t.plan(1)
+  var target = new Target()
+  target.setLevel('DEBUG')
+  t.equal(target.level, 10)
 })
